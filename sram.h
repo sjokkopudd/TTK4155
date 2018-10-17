1727dda4572ad9815eb4a6ead95deb4bf0581317
Nadine <obwallern@gmail.com>
2018-10-17 14:18:03 +0200

    deleted files outside folder
    Signed-off-by: Nadine <obwallern@gmail.com>

--- sram.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void sram_test(void);
-int sram_write(uint16_t address, char data);
-char sram_read(uint16_t address);
--- sram_driver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-
-void sram_test(void);
-int sram_write(uint16_t address, uint8_t data);
-char sram_read(uint16_t address);
