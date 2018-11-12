30708dfbf2994afa0eed0f28d0096e3c5b9b37ad
Nadine <obwallern@gmail.com>
2018-11-12 22:25:46 +0100

    debounce

--- node2/game_controller.c
+++ node2/game_controller.c
@@ -9,10 +9,64 @@
 #include "util/delay.h"
 #include "solenoid_driver.h"
 #include "adc_driver_2.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include "common.h"
 
 static data_t* receive;
 static data_t* message;
 static uint16_t score = 0;
+
+static const uint16_t INIT_DEBOUNCE_VAL = 0xffff;
+
+//current collision state 
+static collision_state curr_coll_state = eNo_Coll;
+
+volatile uint16_t debounce_buffer;
+
+// ----------------------------------------------------------------
+// using timer 1 to debounce the score counting
+// ---------------------------------------------------------------
+static void timer1_init(){
+	//disable global interrupts
+	cli();
+
+	//set clk prescalar fcpu/256
+	set_bit(TCCR1B, CS10);
+	clear_bit(TCCR1B, CS11);
+	clear_bit(TCCR1B, CS12);
+
+	ICR1 = 6250; //timer overflow every 100ms
+
+	//enable global interrupts
+	sei(); 
+
+
+}
+
+static void timer1_start(){
+
+	debounce_buffer = INIT_DEBOUNCE_VAL;
+
+	//enable overflow interrupt
+	set_bit(TIMSK1, TOIE1);
+}
+
+static void timer1_stop(){
+	clear_bit(TIMSK1, TOIE1);
+}
+
+// ---------------------------------------------------------------
+// timer 1 interrupt -> handling debouncing of ir-collision
+// --------------------------------------------------------------
+ISR(TIMER1_OVF_vect){
+	//update debouncing buffer by getting the current ir collision value
+	//store the retrieved value in the buffer
+	debounce_buffer <<= (!check_collision());
+
+}
+
+
 
 static void update_servo(uint8_t val){
 
@@ -32,6 +86,8 @@ void init_game_controller(){
 	receive = malloc(sizeof(data_t));
 	message = malloc(sizeof(data_t));
 	score = 0;
+
+	curr_coll_state = eNo_Coll;
 
 	update_servo(0);
 	solenoid_init();
@@ -82,7 +138,7 @@ void process_game(){
 	}
 
 	//update score only if there is a collision
-	if(check_collision()){
+	/*if(check_collision()){
 		score++;
 
 		if(score & 0xffff){
@@ -95,7 +151,40 @@ void process_game(){
 		can_send_message(message);
 
 	}
+*/
+	//check state of current collision
+	//if there is a collision -> debounce it
+	switch(curr_coll_state){
+		case eNo_Coll:
+			if(check_collision()){
+				//update state
+				curr_coll_state = eDebounce_Coll;
 
+				//reset debouncing buffer, start timer
+				timer1_start();
+
+			}
+			break;
+		case eDebounce_Coll:
+			if(!(debounce_buffer ^ (0xe000))){
+				//is debounced -> increase score
+				score++;
+
+				//stop timer
+				timer1_stop();
+
+				//send score
+				message->data[0] = (score & 0xff);
+				message->data[1] = (score >> 8);
+
+				can_send_message(message);
+
+				curr_coll_state = eNo_Coll;
+			}
+
+			break;
+		default: break;
+	}
 	
 	
 	
--- node2/game_controller.h
+++ node2/game_controller.h
@@ -1,6 +1,13 @@
 #ifndef GAME_CONTROLLER_H
 #define GAME_CONTROLLER_H
 
+typedef enum collision_state
+{
+	eNo_Coll,
+	eDebounce_Coll
+	
+}collision_state;
+
 void init_game_controller();
 void process_game();
 
